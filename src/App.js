0061d14a1ccc69c7f4016121ab2a3b08197d95ea
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-07-08 00:49:49 +0530

    registering users

--- src/App.js
+++ src/App.js
@@ -37,12 +37,29 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+          id: data.id,
+          name: data.name,
+          email: data.email,
+          entries: data.entries,
+          joined: data.joined
+        }});
   }
 
   calculateFaceLocation = (data) => {
@@ -99,7 +116,7 @@ class App extends Component {
             </div>
           : ( this.state.route === 'signin'
               ? <Signin onRouteChange={ this.onRouteChange } />
-              : <Register onRouteChange={ this.onRouteChange } />
+              : <Register loadUser={ this.loadUser } onRouteChange={ this.onRouteChange } />
             )
         }
       </div>
