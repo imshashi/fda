d924c77ff98cb88604c3d1ea575d5ecdb9e7af95
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-07-14 14:13:19 +0530

    connect to live backend

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends Component {
   }
 
   onSubmitRegister = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://fda-api.herokuapp.com/register', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
